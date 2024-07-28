2cfc9dfd2ae191c0adb95e6b89d5846f2b29f04d
unknown <aseeraachu895@gmail.com>
2024-07-28 15:46:09 +0530

    viewall create

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Create from './Components/Create';
+import Viewall from './Components/Viewall';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Route path='/' element={<SignIn/>}/>
       <Route path='/SignUp' element={<SignUp/>}/>
       <Route path='/Create' element={<Create/>}/>
+      <Route path='/Viewall' element={<Viewall/>}/>
+
 
     </Routes>
     </BrowserRouter>
